99f506dc8ba2b5b0cfd8379be2fca348fbd3d779
kingsley <oparaudochukwu277@gmail.com>
2024-03-08 12:16:16 +0000

    initial commit

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,73 @@
+'use client'
+import { Cta, Hero, SectionImageLeft, SectionImageRight } from "@/components/shared/Reusables";
+
+export default function Home() {
+  return (
+    <main className="mx-[50px] mt-[150px] pt-[10]">
+      <section>
+        <Hero 
+          heading=""
+          subheading="Our Mission: Empowering Connections, Ensuring Integrity"
+          paragraphs="Our mission is to empower individuals and organizations by providing a seamless, secure, 
+          and efficient platform for verifiable claims. We're dedicated to simplifying the process of reference checks, 
+          ensuring accuracy, and fostering trust in professional relationships."
+          imgSrc="/assets/images/Mission.png"
+          imgWidth={1200}
+          imgHeight={1200}
+          imgAlt="hero"
+        />
+      </section>
+      <section className="mt-[100px]"> 
+        <SectionImageRight
+          heading="What Sets Us Apart: Automation for a Smarter Future"
+          paragraphs ={
+              <p> 
+                  Veridaq.com stands out with its cutting-edge approach toreference verification. 
+                  By harnessing the power of automation and verifiableclaims, we`re redefining how individuals and 
+                  organizations interact, ensuring afuture where reference checks are both swift and reliable.
+              </p>
+          }
+          imgSrc="/assets/images/Veridaq_Check.png"
+          imgWidth={1200}
+          imgHeight={1200}
+          imgAlt="Veridaq_Check"
+         />
+      </section>
+      <section className="mt-[100px]"> 
+        <SectionImageLeft
+          imgSrc="/assets/images/Values.png"
+          imgWidth={1200}
+          imgHeight={1200}
+          imgAlt="Settings"
+          heading="Our Values: Trust, Integrity, Innovation"
+          paragraphs = {
+            <p>
+                <span className="mt-10">
+                    <span className="font-bold">Trust: </span>Trust is the cornerstone of our platform. We`re committed to building trust through transparent and verifiable processes.
+                  </span>
+                  <br /> <br />
+                  <span className="mt-10">
+                    <span className="font-bold">Integrity: </span>We uphold the highest standards of integrity in all our interactions. Our platform ensures the authenticity of every verifiable claim.
+                  </span>
+                <br /> <br />
+                <span className="mt-10">
+                  <span className="font-bold">Innovation: </span>Innovation is at the heart of what we do. We continuously strive to bring the latest technology to simplify and enhance the reference check experience
+                </span>
+            </p>
+          }
+        />
+      </section>
+      <Cta
+        paragraph = {
+          <p className="text-sm"> 
+            Veridaq.com is more than just a platform; it`s a community dedicated to shaping the future of professional connections. 
+            Join us on this journey of trust, transparency, and efficiency. Whether you`re an individual user or an organization, 
+            Veridaq.com is here to redefine your reference check experience.
+            Ready to experience a smarter, faster, and more secure way of handling reference verification? 
+            Sign Up Now and be part of the Veridaq.com revolution!
+          </p>
+        }
+      />
+    </main>
+  );
+}
---
+++ app/(root)/pricing/page.tsx
@@ -0,0 +1,141 @@
+'use client'
+import Image from "next/image"
+
+import { Card, Checks } from "@/components/shared/Cards"
+import { NoOutlineButton } from "@/components/shared/Button"
+import { Cta, Hero2 } from "@/components/shared/Reusables"
+
+export default function Pricing() {
+    return(
+        <main className="mt-[150px] mx-[50px]">
+            <Hero2
+                heading="Welcome to Veridaq.com, your trusted platform for seamless reference
+                verification. We offer flexible pricing options to meet the needs of
+                both individuals and organizations. Take a look at our pricing plans
+                below:"
+            />
+            <section className="mt-[100px]"> 
+                <p className="font-bold text-[36px] mb-10">For Individuals</p>
+                <div className="flex justify-center gap-10">
+                    <Card 
+                        heading="Freemium Plan"
+                        subheading="Free to request and issue"
+                        cardElements={
+                            <div>
+                                <Checks 
+                                    paragraphs="Work History Veridaq"
+                                />
+                                <Checks 
+                                    paragraphs="Individual Reference or Recommendation Veridaq"
+                                />
+                            </div>
+                        }
+                    />
+                    <Card 
+                        heading="Basic Subscription Plan"
+                        subheading="$4 per annum billing: (N2,400 users in Nigeria)"
+                        cardElements={
+                            <div>
+                                <Checks 
+                                    paragraphs="To access and manage your Work History Veridaq and Individual 
+                                    Reference/Recommendation Veridaq in the Veridaq Repository."
+                                />
+                            </div>
+                        }
+                    />
+                    <Card 
+                        heading="Pay-to-Issue Plan"
+                        subheading="Fees at organisation preferred pricing"
+                        cardElements={
+                            <div>
+                                <Checks 
+                                    paragraphs="Membership Veridaq"
+                                />
+                                <Checks 
+                                    paragraphs="Studentship Status Veridaq"
+                                />
+                                <Checks 
+                                    paragraphs="Document Verification Veridaq"
+                                />
+                            </div>
+                        }
+                    />
+                </div>
+                <div className="mt-10 text-center">
+                    <NoOutlineButton name="Sign Up Now" />
+                </div>
+            </section>
+            <section className="mt-[100px]"> 
+                <p className="font-bold text-[36px] mb-10">For Organizations</p>
+                <div className="flex justify-center gap-10">
+                    <Card 
+                        heading="Freemium Plan"
+                        subheading="Free to Issue and No Earning Plan"
+                        cardElements={
+                            <div>
+                                <Checks 
+                                    paragraphs="Work History Veridaq"
+                                />
+                            </div>
+                        }
+                    />
+                    <Card 
+                        heading="Get Paid"
+                        subheading="Pay to Issue with Earning Plan"
+                        cardElements={
+                            <div>
+                                <Checks 
+                                    paragraphs="Membership Veridaq"
+                                />
+                                <Checks 
+                                    paragraphs="Studentship Status Veridaq"
+                                />
+                                <p className="font-semibold text-[20px] text-center mb-5">75% to Organisation and 25% to Platform Revenue Model</p>
+                                <Checks 
+                                    paragraphs="Document Verification Veridaq"
+                                />
+                            </div>
+                        }
+                    />
+                    <Card 
+                        heading="Enterprise Solutions"
+                        subheading="Contact us for custom pricing tailored to your organization's specific needs."
+                        cardElements={
+                            <div>
+                                {" "}
+                            </div>
+                        }
+                    />
+                </div>
+                <div className="mt-10 text-center">
+                    <NoOutlineButton name="Sign Up Now" />
+                </div>
+            </section>
+            
+            <section className="mt-[100px]">
+                <p className="font-bold text-[36px] mb-10">Additional Information</p>
+                <ul className="text-[24px] list-disc pl-4 ml-8">
+                    <li className="mb-2">
+                        <span className="font-bold"> Billing Cycle: </span>Annual billing for Individual Subscription. Custom billing options available for Enterprise Solutions.
+                    </li>
+                    <li className="mb-2">
+                        <span className="font-bold"> Payment Methods: </span>We accept major credit cardsand electronic transfers.
+                    </li>
+                    <li className="mb-2">
+                        <span className="font-bold"> Customer Support: </span>Our dedicated support team isavailable to assist you with any questions or concerns.
+                    </li>
+                </ul>
+            </section>
+
+            <Cta
+                paragraph = {
+                <p className="text-lg"> 
+                    Start streamlining your reference verification process with Veridaq.com today. Sign Up Now to get started!
+                </p>
+                }
+            />
+
+
+        </main>
+    )
+}
