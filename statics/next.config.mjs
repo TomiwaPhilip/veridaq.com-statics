db3f4e3d000bc50c4bdf8fffcacc9df25fa22d2a
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-03-11 04:17:21 +0000

    updated config

--- statics/next.config.mjs
+++ statics/next.config.mjs
@@ -1,4 +1,12 @@
+// @ts-check
+
 import withPWAInit from "@ducanh2912/next-pwa";
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+};
 
 const withPWA = withPWAInit({
   dest: "public",
