6830d63cafa42c6617216543479978a7fd5ec9ba
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-06-20 03:54:11 +0000

    updated cookie

--- statics/components/shared/CookieNote.tsx
+++ statics/components/shared/CookieNote.tsx
@@ -2,23 +2,25 @@
 
 import { useEffect, useState } from 'react';
 import getSession from '@/lib/session/getSession';
-import useSession from '../session-provider/useSession';
-import { saveSession } from '@/lib/session/saveSession';
+import { saveSession } from '@/lib/session/saveSession'; // Ensure this import is correct
 
 const CookieNote: React.FC = () => {
   const [showCookieNote, setShowCookieNote] = useState(true);
-  const session = useSession();
 
   useEffect(() => {
-    if (session?.isCookieAccepted) {
-      setShowCookieNote(false);
+    async function checkCookie() {
+      const session = await getSession();
+      if (session?.isCookieAccepted) {
+        setShowCookieNote(false);
+      }
     }
-  }, [session]);
+    checkCookie();
+  }, []);
 
   const handleAccept = async () => {
     setShowCookieNote(false);
-    const newSession = {isCookieAccepted: true, isLoggedIn: false,};
-    await saveSession(newSession)
+    const newSession = { isCookieAccepted: true, isLoggedIn: false };
+    await saveSession(newSession);
   };
 
   if (!showCookieNote) {
@@ -28,7 +30,7 @@ const CookieNote: React.FC = () => {
   return (
     <div className="fixed bottom-4 left-4 right-4 bg-[#5c525f] text-white p-4 rounded-lg flex justify-between items-center z-50">
       <p>We use cookies to improve your experience on our site. By using our site, you accept our use of cookies.</p>
-      <button onClick={handleAccept} className="veridaq-gradient text-white py-2 px-4 rounded-md">
+      <button onClick={handleAccept} className="veridaq-gradient from-green-400 to-blue-500 text-white py-2 px-4 rounded-md">
         Okay
       </button>
     </div>
