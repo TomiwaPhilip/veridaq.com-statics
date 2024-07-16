631bd049871c1e6e8235cb4cdad79c7e4cb8de67
chukusuccess <successchuku5@gmail.com>
2024-07-16 10:03:34 +0100

    pricing page recommit

--- statics/app/(root)/pricing/page.tsx
+++ statics/app/(root)/pricing/page.tsx
@@ -1,10 +1,10 @@
-import { Card, Checks } from "@/components/shared/Cards"
-import { NoOutlineButtonBig } from "@/components/shared/Button"
-import { Cta, Hero2 } from "@/components/shared/Reusables"
+import { Card, Checks } from "@/components/shared/Cards";
+import { NoOutlineButtonBig } from "@/components/shared/Button";
+import { Cta, Hero2 } from "@/components/shared/Reusables";
 import {
   BaseFramerAnimation,
   ScrollTriggeredAnimation,
-} from "@/components/shared/Animations"
+} from "@/components/shared/Animations";
 
 export default function Pricing() {
   return (
@@ -35,6 +35,7 @@ export default function Pricing() {
               cardElements={
                 <div>
                   <Checks paragraphs="Work Reference" />
+                  <Checks paragraphs="Hands-on Experience Reference" />
                   <Checks paragraphs="Individual Reference/Recommendation" />
                 </div>
               }
@@ -46,20 +47,31 @@ export default function Pricing() {
                 <div>
                   <Checks
                     paragraphs="
-                            Or get an Access fee WAIVER with Veridaq Reference (available for users in Nigeria).
-                            Veridaq Store is a Veridaq repo to store, 
-                            access and manage all received Work References/Recommendation from Organisations and Individuals."
+                    Veridaq Store is a Veridaq repo to access and manage the received verifiable references/recommendations from Organisations and Individual"
                   />
                 </div>
               }
             />
             <Card
-              heading="Pay-to-Issue Plan"
-              subheading="Fees at Organisation preferred pricing"
+              heading="Verified Account"
+              subheading="$5 (N5000)"
               cardElements={
                 <div>
-                  <Checks paragraphs="Veridaq Reference" />
-                  <Checks paragraphs="Document Verification Veridaq" />
+                  <Checks
+                    paragraphs="
+                    Verified Account grant user's a Veridaq Store Access Fee WAIVER
+(Available for users in Nigeria only)
+"
+                  />
+                </div>
+              }
+            />
+            <Card
+              heading="Pay-for-Issuance and Access Plan"
+              subheading=""
+              cardElements={
+                <div>
+                  <Checks paragraphs="Enterprise's product access comes at the organisation's preferred pricing." />
                 </div>
               }
             />
@@ -88,30 +100,26 @@ export default function Pricing() {
               cardElements={
                 <div>
                   <Checks paragraphs="Work Reference" />
+                  <Checks paragraphs="Hands-on Experience Reference " />
                 </div>
               }
             />
             <Card
-              heading="Get Paid"
-              subheading="Issuing with Earning Plan"
+              heading="Premium Veridaq Store Access Fee
+              $24 (or local currency equivalent)
+              "
+              subheading="Verified Account and other store management perks in addition to freemium access to issuances record."
               cardElements={
                 <div>
-                  <Checks paragraphs="Veridaq Reference (available for users in Nigeria)" />
-                  <p className="font-semibold text-[20px] text-center mb-5">
-                    Price N5,000
-                  </p>
-                  <Checks paragraphs="Document Verification Reference" />
-                  <p className="font-semibold text-[20px] text-center mb-5">
-                    Document Verification Reference: Fee = Issuing Organisation
-                    charge + 50% Markup
-                  </p>
+                  <Checks paragraphs="Work Reference" />
+                  <Checks paragraphs="Hands-on Experience Reference " />
                 </div>
               }
             />
             <Card
-              heading="Enterprise Solutions"
-              subheading="Contact us for custom pricing tailored to your organization's specific needs."
-              cardElements={<div> </div>}
+              heading="Enterprise Plan"
+              subheading="Contact us for custom solutions and pricing tailored to your organisation's specific needs or offering."
+              cardElements={null}
             />
           </div>
         </ScrollTriggeredAnimation>
@@ -150,5 +158,5 @@ export default function Pricing() {
         />
       </ScrollTriggeredAnimation>
     </main>
-  )
+  );
 }
